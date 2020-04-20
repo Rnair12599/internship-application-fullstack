c271e29e52267b35707a62fb7aef10de70b1a823
Rahul Nair <rnair12599@gmail.com>
2020-04-20 03:34:12 -0700

    Added cookies

--- index.js
+++ index.js
@@ -1,12 +1,95 @@
+// Main function that listens for initial request
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+
+// ElementHandler class in HTMLRewriter used to modify elements in html pages
+
+class ElementHandler {
+  element(element) {
+    if(element.tagName === 'title'){
+      console.log(`Incoming element: ${element.tagName}`);
+      element.prepend(`Rahul Nair's Full Stack Assignment`);
+    }
+    else if(element.tagName === 'h1' && (element.getAttribute('id') == "title")  ){
+      console.log(`Incoming element: h1#title`);
+      element.prepend(`Rahul Nair's`);
+      element.append(`Page`);
+    }
+    else if(element.tagName === 'p' && (element.getAttribute('id') == "description")  ){
+      console.log(`Incoming element: p#description`);
+      element.setInnerContent(`for Rahul Nair`);
+    }
+    else if(element.tagName === 'a' && (element.getAttribute('id') == "url")  ){
+      console.log(`Incoming element: a#urls`);
+      element.setAttribute("href", "https://github.com/Rnair12599")
+      element.setInnerContent(`Check out Rahul Nair's github!`);
+    }
+  }
+
+  comments(comment) {
+    // An incoming comment
+  }
+
+  text(text) {
+    // An incoming piece of text
+
+  }
+}
+
+
+
+// request handler that also fetch's other pages and return response with appropriate html page
+
+
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  let cookieVal = request.headers.get('cookie');
+  console.log(cookieVal);
+
+  const response = await fetch('https://cfw-takehome.developers.workers.dev/api/variants', {
+    headers: {
+      'Content-Type': 'text/html',
+    },
+  });
+
+  let urls = await response.json();
+
+  let val = cookieVal;
+
+  if(cookieVal && cookieVal.includes("variant=0") ){
+    val = 0;
+  }
+  else if(cookieVal && cookieVal.includes("variant=1") ){
+    val = 1;
+  }
+  else{
+    let d = new Date();
+    val = d.getTime() % 2;
+  }
+
+
+  const varResponse = await fetch(urls.variants[val], {
+    headers: {
+      'Content-Type': 'text/html',
+    },
+  });
+
+
+  let htmlrewriter =  new HTMLRewriter()
+    .on('title', new ElementHandler())
+    .on('h1#title', new ElementHandler())
+    .on('p#description', new ElementHandler())
+    .on('a#url', new ElementHandler())
+    .transform(varResponse);
+  
+
+
+  newPage = htmlrewriter.body;
+  return new Response(newPage, {
+    headers: {
+      'Content-Type': 'text/html',
+      'Set-Cookie' :`variant=${val}`,
+    },
+  });
+
 }
